35312ceddd1b061d49e989f58b71e116a30bdc46
Nhan Vi Nam <nhanvinam99@gmail.com>
2020-05-08 08:22:34 +0700

    Finish banner

--- src/components/HomeContainer/BannerComponent/index.js
+++ src/components/HomeContainer/BannerComponent/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
-// import { classPrefixor } from '../../../utils/classPrefixor';
-
+import React from "react";
+import { classPrefixor } from "../../../utils/classPrefixor";
+import { Container, Row, Col } from "react-bootstrap";
 const BannerComponent = () => {
-    const prefix = "banner";
-    // const c = classPrefixor(prefix);
-    return (
-        <section className = {prefix}>
-            
-        </section>
-    )
-}
+  const prefix = "banner";
+  const c = classPrefixor(prefix);
+  return (
+    <section className={prefix}>
+      <Container>
+        <Row>
+          <Col lg="5">
+            <div className={c`left`}>
+              <div className="left__child">
+                <h1>Take the first step to learn with us</h1>
+                <p>
+                  In the history of modern astronomy, there is probably no one
+                  greater leap forward than the building and launch of the space
+                  telescope known as the Hubble.
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col lg={{ span: 5, offset: 2 }}>
+              <div className={c`right`}>
+              <img src="./img/header-img.png" alt=""/>
 
-export default BannerComponent
+              </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default BannerComponent;
