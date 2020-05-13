1e12078d88cae1388e7dedcac5a6757e4f5f225d
Nhan Vi Nam <nhanvinam99@gmail.com>
2020-05-14 01:45:39 +0700

    Finish page not found

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 const PageNotFound = () => {
-    return (
-        <>
-         <h1>Page not Found</h1>   
-        </>
-    )
-}
+  const prefix = "pageNotFound";
+  return (
+    <>
+      <section className={prefix}>
+        <Container>
+          <NavLink exact to="/">
+            Back to Home
+          </NavLink>
+          <img
+            src="./img/95592222_939910453112947_2297522592409452544_o.jpg"
+            alt="img"
+          />
+        </Container>
+      </section>
+    </>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
